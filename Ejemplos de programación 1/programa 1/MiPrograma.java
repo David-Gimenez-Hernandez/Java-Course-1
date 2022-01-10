dad2a7f87a508668b3b6b62e4f104eeadfb905d0
David Gimenez <93078743+David-Gimenez-Hernandez@users.noreply.github.com>
2022-01-10 11:54:46 +0100

    Delete Ejemplos de programación 1 directory

--- Ejemplos de programación 1/programa 1/MiPrograma.java
+++
@@ -1,7 +0,0 @@
-/* David Giménez Hernández
- * 210311*/
-public class MiPrograma{
-  public static void main(String[] args){
-    System.out.println("Hola Mundo");
-  }//de main
-}//de programa1HelloWorld
